2d4004d7acff1422481112921dd0245e0999d818
hl_tang <hltang@protonmail.com>
2024-03-17 14:47:07 +0900

    almost finsih owner hard coded

--- front-end/src/components/OwnerSideBar.vue
+++ front-end/src/components/OwnerSideBar.vue
@@ -5,7 +5,7 @@
   <!-- Sidebar -->
   <div class="bg-indigo-800 text-white w-64">
     <div class="p-4">
-      <h1 class="text-2xl font-bold">Sidebar</h1>
+      <h1 class="text-2xl font-bold">駐車管理</h1>
     </div>
     <div>
       <ul class="py-4">
@@ -17,7 +17,7 @@
           <router-link to="/owner/ownerlist">駐車所リスト一覧</router-link>
         </li>
         <li class="px-4 py-2 hover:bg-indigo-700 focus:bg-indigo-700">
-          <router-link to="/owner">売上確認</router-link>
+          <router-link to="/owner/statistics">売上確認</router-link>
         </li>
 
       </ul>
--- front-end/src/pages/OwnerList.vue
+++ front-end/src/pages/OwnerList.vue
@@ -8,96 +8,233 @@ const search = ref('')
 const headers = [
   {
     align: 'start',
-    key: 'name',
+    key: 'parking_lot_name',
     sortable: false,
-    title: 'Dessert (100g serving)',
+    title: '駐車場名前',
   },
-  { key: 'calories', title: 'Calories' },
-  { key: 'fat', title: 'Fat (g)' },
-  { key: 'carbs', title: 'Carbs (g)' },
-  { key: 'protein', title: 'Protein (g)' },
-  { key: 'iron', title: 'Iron (%)' },
+  { key: 'address', title: '場所' },
+  { key: 'month_fee', title: '月額料金' },
+  { key: 'user_count', title: '利用者数' },
+  { key: 'park_limit', title: '最大利用者上限' },
+  { key: 'user_rate', title: '利用率 (%)' },
+  { key: 'month_profit', title: '月分売上（円）' },
 ]
-const desserts = [
+const pk_list = [
   {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6,
-    carbs: 24,
-    protein: 4,
-    iron: 1,
+    "_id": "65f66895674d5bcdf4d5e0e5",
+    "parking_lot_name": "新宿駐輪場",
+    "address": "東京都新宿",
+    "user_list": [
+      "65f66930674d5bcdf4d5e0f1",
+      "65f67d19549e095e47e4f77b"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 500,
+    "user_count": 2,
+    "park_limit": 60,
+    "user_rate": 3.3333333333333335,
+    "month_profit": 1000,
+    "__v": 0
   },
   {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9,
-    carbs: 37,
-    protein: 4.3,
-    iron: 1,
+    "_id": "65f668b3674d5bcdf4d5e0e8",
+    "parking_lot_name": "渋谷駐輪場",
+    "address": "東京都渋谷",
+    "user_list": [
+      "65f6694d674d5bcdf4d5e0f7"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 1000,
+    "user_count": 1,
+    "park_limit": 60,
+    "user_rate": 1.6666666666666667,
+    "month_profit": 1000,
+    "__v": 0
   },
   {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16,
-    carbs: 23,
-    protein: 6,
-    iron: 7,
+    "_id": "65f668ce674d5bcdf4d5e0eb",
+    "parking_lot_name": "市ヶ谷駐輪場",
+    "address": "東京都市ヶ谷",
+    "user_list": [
+      "65f66967674d5bcdf4d5e0ff"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 2000,
+    "user_count": 1,
+    "park_limit": 10,
+    "user_rate": 10,
+    "month_profit": 2000,
+    "__v": 0
   },
   {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    iron: 8,
+    "_id": "65f68163549e095e47e4f785",
+    "parking_lot_name": "渋谷駅新南口 駐輪場",
+    "address": "渋谷3-16-1",
+    "user_list": [
+      "65f6822a549e095e47e4f791",
+      "65f68231549e095e47e4f797",
+      "65f68251549e095e47e4f79f",
+      "65f68259549e095e47e4f7a5"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 300,
+    "user_count": 4,
+    "park_limit": 50,
+    "user_rate": 8,
+    "month_profit": 1200,
+    "__v": 0
   },
   {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16,
-    carbs: 49,
-    protein: 3.9,
-    iron: 16,
+    "_id": "65f68194549e095e47e4f788",
+    "parking_lot_name": "渋谷道玄坂駐輪場",
+    "address": "東京都渋谷区道玄坂１丁目１８",
+    "user_list": [
+      "65f6826e549e095e47e4f7ab",
+      "65f68274549e095e47e4f7b1",
+      "65f6827a549e095e47e4f7b7",
+      "65f68286549e095e47e4f7bd"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 800,
+    "user_count": 4,
+    "park_limit": 50,
+    "user_rate": 8,
+    "month_profit": 3200,
+    "__v": 0
   },
   {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0,
-    carbs: 94,
-    protein: 0,
-    iron: 0,
+    "_id": "65f681d5549e095e47e4f78b",
+    "parking_lot_name": "渋谷ストリーム 自転車駐輪場",
+    "address": "東京都渋谷区渋谷３丁目２１−３",
+    "user_list": [],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 1200,
+    "user_count": 0,
+    "park_limit": 30,
+    "user_rate": 0,
+    "month_profit": 0,
+    "__v": 0
   },
   {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    iron: 2,
+    "_id": "65f681fe549e095e47e4f78e",
+    "parking_lot_name": "渋谷駅東口地下自転車駐輪場",
+    "address": "東京都渋谷区渋谷３丁目１９",
+    "user_list": [],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 700,
+    "user_count": 0,
+    "park_limit": 200,
+    "user_rate": 0,
+    "month_profit": 0,
+    "__v": 0
   },
   {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    iron: 45,
+    "_id": "65f66895674d5bcdf4d5e0e5",
+    "parking_lot_name": "新宿駐輪場",
+    "address": "東京都新宿",
+    "user_list": [
+      "65f66930674d5bcdf4d5e0f1",
+      "65f67d19549e095e47e4f77b"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 500,
+    "user_count": 2,
+    "park_limit": 60,
+    "user_rate": 3.3333333333333335,
+    "month_profit": 1000,
+    "__v": 0
   },
   {
-    name: 'Donut',
-    calories: 452,
-    fat: 25,
-    carbs: 51,
-    protein: 4.9,
-    iron: 22,
+    "_id": "65f668b3674d5bcdf4d5e0e8",
+    "parking_lot_name": "渋谷駐輪場",
+    "address": "東京都渋谷",
+    "user_list": [
+      "65f6694d674d5bcdf4d5e0f7"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 1000,
+    "user_count": 1,
+    "park_limit": 60,
+    "user_rate": 1.6666666666666667,
+    "month_profit": 1000,
+    "__v": 0
   },
   {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26,
-    carbs: 65,
-    protein: 7,
-    iron: 6,
+    "_id": "65f668ce674d5bcdf4d5e0eb",
+    "parking_lot_name": "市ヶ谷駐輪場",
+    "address": "東京都市ヶ谷",
+    "user_list": [
+      "65f66967674d5bcdf4d5e0ff"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 2000,
+    "user_count": 1,
+    "park_limit": 10,
+    "user_rate": 10,
+    "month_profit": 2000,
+    "__v": 0
+  },
+  {
+    "_id": "65f68163549e095e47e4f785",
+    "parking_lot_name": "渋谷駅新南口 駐輪場",
+    "address": "渋谷3-16-1",
+    "user_list": [
+      "65f6822a549e095e47e4f791",
+      "65f68231549e095e47e4f797",
+      "65f68251549e095e47e4f79f",
+      "65f68259549e095e47e4f7a5"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 300,
+    "user_count": 4,
+    "park_limit": 50,
+    "user_rate": 8,
+    "month_profit": 1200,
+    "__v": 0
+  },
+  {
+    "_id": "65f68194549e095e47e4f788",
+    "parking_lot_name": "渋谷道玄坂駐輪場",
+    "address": "東京都渋谷区道玄坂１丁目１８",
+    "user_list": [
+      "65f6826e549e095e47e4f7ab",
+      "65f68274549e095e47e4f7b1",
+      "65f6827a549e095e47e4f7b7",
+      "65f68286549e095e47e4f7bd"
+    ],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 800,
+    "user_count": 4,
+    "park_limit": 50,
+    "user_rate": 8,
+    "month_profit": 3200,
+    "__v": 0
+  },
+  {
+    "_id": "65f681d5549e095e47e4f78b",
+    "parking_lot_name": "渋谷ストリーム 自転車駐輪場",
+    "address": "東京都渋谷区渋谷３丁目２１−３",
+    "user_list": [],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 1200,
+    "user_count": 0,
+    "park_limit": 30,
+    "user_rate": 0,
+    "month_profit": 0,
+    "__v": 0
+  },
+  {
+    "_id": "65f681fe549e095e47e4f78e",
+    "parking_lot_name": "渋谷駅東口地下自転車駐輪場",
+    "address": "東京都渋谷区渋谷３丁目１９",
+    "user_list": [],
+    "owner_id": "65f66873674d5bcdf4d5e0e3",
+    "month_fee": 700,
+    "user_count": 0,
+    "park_limit": 200,
+    "user_rate": 0,
+    "month_profit": 0,
+    "__v": 0
   },
 ]
 
@@ -116,13 +253,13 @@ const desserts = [
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.</p>
       </div> -->
 
-      <v-card title="Nutrition" flat>
+      <v-card flat>
         <template v-slot:text>
           <v-text-field v-model="search" label="Search" prepend-inner-icon="mdi-magnify" variant="outlined" hide-details
             single-line></v-text-field>
         </template>
 
-        <v-data-table :headers="headers" :items="desserts" :search="search"></v-data-table>
+        <v-data-table :headers="headers" :items="pk_list" :search="search"></v-data-table>
       </v-card>
     </div>
   </div>
